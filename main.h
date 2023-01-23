6c9b2f84f696f7037fe71bc7679c299c93dc9a82
Skima1 <ayodeleoloniyo@gmail.com>
2023-01-23 13:19:04 -0800

    #include main.h

--- main.h
+++ main.h
@@ -31,7 +31,6 @@
 #define S_SHORT 1
 
 /**
- *
  * struct fmt - Struct op
  *
  * @fmt: The format.
@@ -41,7 +40,6 @@
  */
 
 struct fmt
-
 {
 
 	char fmt;
@@ -50,7 +48,6 @@ struct fmt
 };
 
 /**
- *
  * typedef struct fmt fmt_t - Struct op
  *
  * @fmt: The format.
@@ -63,7 +60,7 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 
-int handle_print(const char *fmt, int *i,
+int handle_print(const char *fmt, int *i;
 
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
@@ -73,41 +70,41 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 int print_char(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_string(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_percent(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
 
 int print_int(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_binary(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_unsigned(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_octal(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_hexadecimal(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_hexa_upper(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
 
@@ -117,13 +114,13 @@ int print_hexa(va_list types, char map_to[],
 
 int print_non_printable(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /* Funcion to print memory address */
 
 int print_pointer(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /* Funciotns to handle other specifiers */
 
@@ -139,37 +136,37 @@ int get_size(const char *format, int *i);
 
 int print_reverse(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /*Function to print a string in rot 13*/
 
 int print_rot13string(va_list types, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /* width handler */
 
 int handle_write_char(char c, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int write_number(int is_positive, int ind, char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int write_num(int ind, char bff[], int flags, int width, int precision,
 
-		        int length, char padd, char extra_c);
+		int length, char padd, char extra_c);
 
 int write_pointer(char buffer[], int ind, int length,
 
-		        int width, int flags, char padd, char extra_c, int padd_start);
+		int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
 
 		char buffer[],
 
-		        int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
 
