bbce0088c88b40beb0c5bce318778f70bc3e0d78
Oloniyo Sunday <ayodeleoloniyo@gmail.com>
2023-01-25 03:46:51 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,11 +13,11 @@ int main(void)
 	void *addr;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
-	len 2 = printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len 2, len 2);
+	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
 	_printf("Unsigned:[%u]\n", ui);
@@ -33,9 +33,9 @@ int main(void)
 	_printf("Address:[%p]\n", addr);
 	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
-	len 2 = printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len 2);
+	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 	return (0);
